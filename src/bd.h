f435768744d7cef6b254449a00e43f7c51028246
Jingyu He <jingyuhe@hotmail.com>
2020-01-18 22:03:56 -0600

    my own copy of bcf2, initialization

---
+++ src/bd.h
@@ -0,0 +1,16 @@
+#ifndef GUARD_bd_h
+#define GUARD_bd_h
+
+#include "rng.h"
+#include "info.h"
+#include "tree.h"
+#include "logging.h"
+
+
+#ifdef MPIBART
+bool bd(tree& x, xinfo& xi, pinfo& pi, RNG& gen, size_t numslaves);
+#else
+bool bd(tree& x, xinfo& xi, dinfo& di, double* phi, pinfo& pi, RNG& gen, Logger logger);
+#endif
+
+#endif
