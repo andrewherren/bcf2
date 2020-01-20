a45a70d05fb54f3f4d9b504b6ca9ff88c6c1ba69
Jingyu He <jingyuhe@hotmail.com>
2020-01-20 18:09:00 +0800

    at least no memory leaking

--- src/bcf_clean_overpar_ini.cpp
+++ src/bcf_clean_overpar_ini.cpp
@@ -174,17 +174,59 @@ List bcfoverparRcppClean_ini(SEXP treedraws_con, SEXP treedraws_mod, double musc
   //  Rcout <<"\nburn,nd,number of trees: " << burn << ", " << nd << ", " << m << endl;
   //  Rcout <<"\nlambda,nu,kfac: " << lambda << ", " << nu << ", " << kfac << endl;
 
+cout << "before loading trees " << endl;
+  // load trees
+  Rcpp::CharacterVector itrees_con(Rcpp::wrap(treedraws_con));
+  Rcpp::CharacterVector itrees_mod(Rcpp::wrap(treedraws_mod));
+
+  std::string itv_con(itrees_con[0]);
+  std::stringstream ttss_con(itv_con);
+  size_t mm_con, pp_con;
+  ttss_con >> mm_con >> pp_con;
+  std::vector<tree> t_con(mm_con);
+  for (size_t j = 0; j < mm_con; j++)
+  {
+    ttss_con >> t_con[j];
+  }
+
+cout << "load con trees " << endl;
+
+  std::string itv_mod(itrees_mod[0]);
+  std::stringstream ttss_mod(itv_mod);
+  size_t mm_mod, pp_mod;
+  ttss_mod >> mm_mod >> pp_mod;
+  std::vector<tree> t_mod(mm_mod);
+  for (size_t j = 0; j < mm_mod; j++)
+  {
+    ttss_mod >> t_mod[j];
+  }
+
+cout << "load all trees " << endl;
+
+
+
+cout << "print con trees " << endl;
+for(size_t tt = 0; tt < mm_con; tt ++ ){
+  cout << "index " << tt << endl;
+  cout << t_con[tt] << endl;
+}
+
+cout << "print mod trees " << endl;
+for(size_t tt = 0; tt < mm_mod; tt ++ ){
+  cout << "index " << tt << endl;
+  cout << t_mod[tt] << endl;
+}
   /*****************************************************************************
   /* Setup the model
   *****************************************************************************/
   //--------------------------------------------------
   //trees
   //make trt_init a parameter later
-  std::vector<tree> t_mod(ntree_mod);
-  for(size_t i=0;i<ntree_mod;i++) t_mod[i].setm(trt_init/(double)ntree_mod);
+  // std::vector<tree> t_mod(ntree_mod);
+  // for(size_t i=0;i<ntree_mod;i++) t_mod[i].setm(trt_init/(double)ntree_mod);
 
-  std::vector<tree> t_con(ntree_con);
-  for(size_t i=0;i<ntree_con;i++) t_con[i].setm(ybar/(double)ntree_con);
+  // std::vector<tree> t_con(ntree_con);
+  // for(size_t i=0;i<ntree_con;i++) t_con[i].setm(ybar/(double)ntree_con);
 
   //--------------------------------------------------
   //prior parameters
@@ -219,32 +261,81 @@ List bcfoverparRcppClean_ini(SEXP treedraws_con, SEXP treedraws_mod, double musc
 
   double sigma = shat;
 
-  // @Peter This is where dinfo is initialized
+//   // @Peter This is where dinfo is initialized
 
-  //--------------------------------------------------
-  //dinfo for control function m(x)
-//  Rcout << "ybar " << ybar << endl;
+//   //--------------------------------------------------
+//   //dinfo for control function m(x)
+// //  Rcout << "ybar " << ybar << endl;
+//   double* allfit_con = new double[n]; //sum of fit of all trees
+//   for(size_t i=0;i<n;i++) allfit_con[i] = ybar;
+//   double* r_con = new double[n]; //y-(allfit-ftemp) = y-allfit+ftemp
+//   dinfo di_con;
+//   di_con.n=n; 
+//   di_con.p = p_con; 
+//   di_con.x = &x_con[0]; 
+//   di_con.y = r_con; //the y for each draw will be the residual
+//   di_con.w = w;
+
+//   //--------------------------------------------------
+//   //dinfo for trt effect function b(x)
+//   double* allfit_mod = new double[n]; //sum of fit of all trees
+//   for(size_t i=0;i<n;i++) allfit_mod[i] = (z_[i]*bscale1 + (1-z_[i])*bscale0)*trt_init;
+//   double* r_mod = new double[n]; //y-(allfit-ftemp) = y-allfit+ftemp
+//   dinfo di_mod;
+//   di_mod.n=n; 
+//   di_mod.p=p_mod; 
+//   di_mod.x = &x_mod[0]; 
+//   di_mod.y = r_mod; //the y for each draw will be the residual
+//   di_mod.w = w;
+
+  // allocate storage
+  double* ftemp  = new double[n]; //fit of current tree
+  double* precs = new double[n]; // temp storage for conditional ''precisions'' in heteroskedastic updates
   double* allfit_con = new double[n]; //sum of fit of all trees
-  for(size_t i=0;i<n;i++) allfit_con[i] = ybar;
-  double* r_con = new double[n]; //y-(allfit-ftemp) = y-allfit+ftemp
-  dinfo di_con;
-  di_con.n=n; 
-  di_con.p = p_con; 
-  di_con.x = &x_con[0]; 
-  di_con.y = r_con; //the y for each draw will be the residual
-  di_con.w = w;
-
-  //--------------------------------------------------
-  //dinfo for trt effect function b(x)
   double* allfit_mod = new double[n]; //sum of fit of all trees
-  for(size_t i=0;i<n;i++) allfit_mod[i] = (z_[i]*bscale1 + (1-z_[i])*bscale0)*trt_init;
+  double* allfit = new double[n]; //yhat
+  double* r_con = new double[n]; //y-(allfit-ftemp) = y-allfit+ftemp
   double* r_mod = new double[n]; //y-(allfit-ftemp) = y-allfit+ftemp
+  dinfo di_con;
+  di_con.n=n; di_con.p=p_con; di_con.x = &x_con[0];  //the y for each draw will be the residual
   dinfo di_mod;
-  di_mod.n=n; 
-  di_mod.p=p_mod; 
-  di_mod.x = &x_mod[0]; 
-  di_mod.y = r_mod; //the y for each draw will be the residual
+  di_mod.n=n; di_mod.p=p_mod; di_mod.x = &x_mod[0];  //the y for each draw will be the residual
+  di_con.w = w;
   di_mod.w = w;
+
+  // initialize allfit, residuals from trees read in 
+  for(size_t kk = 0; kk < n; kk ++ ){
+    allfit[kk] = 0;
+    allfit_con[kk] = 0;
+    allfit_mod[kk] = 0;
+  }
+
+  for(size_t j=0;j<ntree_con;j++) {
+    fit(t_con[j],xi_con,di_con,ftemp);
+    for(size_t k=0;k<n;k++) {
+      allfit[k] += mscale*ftemp[k];
+      allfit_con[k] += mscale*ftemp[k];
+      r_con[k] = (y[k]-allfit[k])/mscale;
+    }
+  }
+
+  for(size_t j=0;j<ntree_mod;j++) 
+  {
+    fit(t_mod[j],xi_mod,di_mod,ftemp);
+    for(size_t k=0;k<ntrt;k++) {
+      allfit[k] += bscale1*ftemp[k];
+      allfit_mod[k] += bscale1*ftemp[k];
+      r_mod[k] = (y[k]-allfit[k])/bscale1;
+    }
+    for(size_t k=ntrt;k<n;k++) {
+      allfit[k] += bscale0*ftemp[k];
+      allfit_mod[k] += bscale0*ftemp[k];
+      r_mod[k] = (y[k]-allfit[k])/bscale1;
+    }
+  }
+
+  di_con.y=r_con;
+  di_mod.y=r_mod;
 
   //--------------------------------------------------
   //dinfo and design for trt effect function out of sample
@@ -311,12 +402,12 @@ List bcfoverparRcppClean_ini(SEXP treedraws_con, SEXP treedraws_mod, double musc
 
   //--------------------------------------------------
   //storage for the fits
-  double* allfit = new double[n]; //yhat
+  // double* allfit = new double[n]; //yhat
   for(size_t i=0;i<n;i++) {
     allfit[i] = allfit_mod[i] + allfit_con[i];
     if(randeff) allfit[i] += allfit_random[i];
   }
-  double* ftemp  = new double[n]; //fit of current tree
+  // double* ftemp  = new double[n]; //fit of current tree
 
   NumericVector sigma_post(nd);
   NumericVector msd_post(nd);
